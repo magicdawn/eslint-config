99be380aa481d2f465a454b3854238f851bfb049
magicdawn <magicdawn@qq.com>
2025-05-18 18:03:56 +0800

    feat: more fromSxzz options

--- src/configs/sxzz.ts
+++ src/configs/sxzz.ts
@@ -1,13 +1,28 @@
 import { GLOB_SRC, GLOB_TS, GLOB_TSX, sxzz } from '@sxzz/eslint-config'
-import { uniq } from 'es-toolkit'
+import { invariant, uniq } from 'es-toolkit'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import { mgCustomIgnoreConfig, mgCustomJsonOrder, mgCustomRules } from '../mg-custom'
+import type { Linter } from 'eslint'
 
-export function fromSxzz(...args: Parameters<typeof sxzz>) {
-  const [moreOptions, ...moreConfigs] = args
+type SxzzArgs = Parameters<typeof sxzz>
+type SxzzOptions = SxzzArgs[0]
+type Rest<T extends any[]> = T extends [any?, ...infer B] ? B : []
+type UserConfig = Rest<SxzzArgs>[number]
+
+type SortImportOptions = {
+  internalPattern?: string[]
+  groups?: string[]
+}
+
+interface MixedOptions extends Exclude<SxzzOptions, undefined> {
+  sortImport?: SortImportOptions
+}
+
+export function fromSxzz(options?: MixedOptions, ...moreConfigs: UserConfig[]) {
+  const { sortImport, ...sxzzOptions } = options ?? {}
   return (
     sxzz(
-      { command: false, prettier: false, ...(moreOptions ?? {}) },
+      { command: false, prettier: false, ...(sxzzOptions ?? {}) },
       [
         eslintConfigPrettier,
         mgCustomIgnoreConfig,
@@ -52,8 +67,26 @@ export function fromSxzz(...args: Parameters<typeof sxzz>) {
             if (ruleOption) ruleOption.order = uniq([...target.order, ...ruleOption.order])
           }
         }
+        return config
+      })
 
+      // replace ts import order
+      .override('sxzz/sort/imports', (config) => {
+        // from options
+        const { internalPattern, groups } = sortImport ?? {}
+
+        const val = config.rules?.['perfectionist/sort-imports']
+        if (val && Array.isArray(val) && val.length > 0) {
+          const [severity, ...options] = val
+          invariant(options.length === 1, 'options.length should be 1')
+          // modify options
+          options[0].internalPattern = uniq(['^[~@#$]/.*', String.raw`^\$.+`, ...(internalPattern ?? [])]) // add `$`
+          options[0].groups = uniq([...(groups ?? []), 'side-effect-style', 'side-effect', ...options[0].groups])
+        }
         return config
       })
   )
 }
+
+export type GetRuleOptions<T extends Linter.RuleEntry> =
+  T extends Linter.RuleSeverityAndOptions<infer Options> ? Options : never
